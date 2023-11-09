b091668d7d1ab69eed2faf69f90da84c79e8b7e0
subashhB <subashbaidya9@gmail.com>
2023-11-09 12:04:16 +0545

    More on Controllers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,10 @@ class HomeController < ApplicationController
   end
   
   def about
+    # Instance Varible that would work everywhere in the app
+    @about_me = "My name is ........."
+
+    # Scop variable that would only work in the method
+    about_me = "This won't work anywhere else"
   end
 end
